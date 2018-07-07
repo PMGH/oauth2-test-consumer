f1e6f8d9e500b7252d7b8f307439816f932ff826
Peter McCready <petermccready2@gmail.com>
2018-07-07 23:33:57 +0100

    set user on consumer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
   def authentication_callback
     auth = request.env['omniauth.auth']
+    set_user(auth)
     render json: auth
+  end
+
+  private
+
+  def set_user auth
+    user = {
+      id: auth['uid'],
+      name: auth['info']['name'],
+      email: auth['info']['email']
+    }
+    User.find_or_create_by(user)
   end
 end

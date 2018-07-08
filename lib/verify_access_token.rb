5a2d74f569ec2d1f3ea34e159ad323d54e5829db
Peter McCready <petermccready2@gmail.com>
2018-07-08 23:16:10 +0100

    use local_env values for JWT secret and algorithm

--- lib/verify_access_token.rb
+++ lib/verify_access_token.rb
@@ -1,5 +1,5 @@
 def verify_access_token
   token = cookies[:access_token]
-  jwt = JWT.decode token, "MY-SECRET", true, { algorithm: 'HS512' } if token
+  jwt = JWT.decode token, ENV['SECRET'], true, { algorithm: ENV['ALGORITHM'] } if token
   redirect_to "http://localhost:3001/auth/doorkeeper" if !jwt
 end

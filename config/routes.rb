5a3ecca6c338204e25f1e1adc1142639f129ea87
Peter McCready <petermccready2@gmail.com>
2018-07-08 23:17:03 +0100

    use constraints to redirect to auth/doorkeeper if no access_token cookie

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # redirect root ('localhost:3001/') to '/auth/doorkeeper' - omniauth strategy if no access_token cookie
-  root to: redirect('/auth/doorkeeper')
+  root to: redirect('/auth/doorkeeper'), constraints: lambda { |request| !request.cookies['access_token'] } # !cookies[:access_token]
 
   # get '/auth/:provider/callback' => 'application#authentication_callback'
   get '/auth/doorkeeper/callback' => 'application#authentication_callback'

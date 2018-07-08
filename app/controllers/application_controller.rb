a967dc4547182c17a3b64cfa25550406b2ad6255
Peter McCready <petermccready2@gmail.com>
2018-07-08 19:30:37 +0100

    add jwt to cookies and decode jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
+require 'jwt'
+
 class ApplicationController < ActionController::Base
+  before_action :verify_access_token
+
   def authentication_callback
     auth = request.env['omniauth.auth']
     set_user(auth)
@@ -14,5 +18,12 @@ class ApplicationController < ActionController::Base
       email: auth['info']['email']
     }
     User.find_or_create_by(user)
+    cookies.permanent[:access_token] = auth['credentials']['token']
+  end
+
+  def verify_access_token
+    token = cookies[:access_token]
+    jwt = JWT.decode token, "MY-SECRET", true, { algorithm: 'HS512' } if token
+    redirect_to root if !jwt
   end
 end

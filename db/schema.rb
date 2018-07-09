70e41dabd7f5a8052163645b353d8901e673352c
Peter McCready <peter.mccready@castlightfinancial.com>
2018-07-09 16:59:07 +0100

    update schema for tests model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_07_220401) do
+ActiveRecord::Schema.define(version: 2018_07_09_141340) do
+
+  create_table "tests", force: :cascade do |t|
+    t.string "name"
+    t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"

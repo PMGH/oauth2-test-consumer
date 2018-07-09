01fd171d4ebc8e5b7ade8c242f6ee52b3af63d69
Peter McCready <peter.mccready@castlightfinancial.com>
2018-07-09 16:58:53 +0100

    remove comment

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
 
   # redirect root ('localhost:3001/') to '/auth/doorkeeper' - omniauth strategy if no access_token cookie
-  root to: redirect('/auth/doorkeeper'), constraints: lambda { |request| !request.cookies['access_token'] } # !cookies[:access_token]
+  root to: redirect('/auth/doorkeeper'), constraints: lambda { |request| !request.cookies['access_token'] }
 
   # get '/auth/:provider/callback' => 'application#authentication_callback'
   get '/auth/doorkeeper/callback' => 'application#authentication_callback'

54fe0ac198d0232177751cd652cabdf24ef285fb
Peter McCready <peter.mccready@castlightfinancial.com>
2018-07-09 17:29:44 +0100

    add test controller actions

--- app/controllers/api/test_controller.rb
+++ app/controllers/api/test_controller.rb
@@ -1,7 +1,37 @@
 class Api::TestController < Api::ApplicationController
   def index
-    render json: { value: "json!" }
+    @users = User.all
+    render json: @users
   end
+
   def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
